91ee01a18ccef99a979408a3a4aa185dac9ef6c4
Yvon <mutuyeyezuyvon1@gmail.com>
2021-07-11 10:42:00 +0200

    get methods in web app

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-import tweepy
-import csv
-import pandas as pd
-
-access_token = ""
-access_token_secret = ""
-api_key = ""
-api_key_secret = ""
+import twint
+from datetime import datetime, timedelta
 
 
 def f_access():
-    auth = tweepy.OAuthHandler(api_key, api_key_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True)
+    c = twint.Config()
+    three_days_ago = (datetime.now() - timedelta(3)).strftime('%Y-%m-%d')
     search_key = "request for startup"
-    csv_file = open(search_key + ".csv", 'a+', newline='', encoding="utf-8")
-    csvWriter = csv.writer(csv_file)
-    created = []
-    id = []
-    user_name = []
-    text = []
-    for tweet in tweepy.Cursor(api.search, q=search_key, count=1000, lang='id', since='2021-07-05',
-                               until='2021-07-08').items():
-        print(tweet.created_at, tweet.id, tweet.user.name, tweet.text)
-        created.append(tweet.created_at)
-        id.append(tweet.id)
-        user_name.append(tweet.user.name)
-        text.append(tweet.text.encode('utf-8'))
-        tweets = [tweet.created_at, tweet.id, tweet.user.name, tweet.text.encode('utf-8')]
-        csvWriter.writerow(tweets)
-    dictTweets = {"Date": created, 'id': id, 'username': user_name, 'text': text}
-    df = pd.DataFrame(dictTweets, columns=['Date', 'id', 'username', 'txt'])
-    df
+    c.Search = search_key
+    c.Since = three_days_ago
+    c.Format = "User: @{username} | Date: {date} |Tweet: {tweet} |Likes: {likes} |RT: {retweets} |Replies: {replies} " \
+               "\n "
+    c.Store_csv = True
+    c.Custom['tweet'] = ['username', 'date', 'tweet', 'likes_count', 'retweets_count', 'replies_count']
+    c.Output = search_key + ".csv"
+    twint.run.Search(c)
+    print('==============================================================')
+    print("View in Web application navigate to view_web_app then run it")
+    print('==============================================================')
 
 
 if __name__ == '__main__':
--- view_web_app.py
+++ view_web_app.py
@@ -1,19 +1,15 @@
 import pandas
 from flask import Flask, render_template
-import tablib
-import os
+
 
 app = Flask(__name__)
-# dataset = tablib.Dataset()
-# with open(os.path.join(os.path.dirname(__file__), 'request for startup.csv')) as f:
-#     dataset.csv = f.read()
 
 
 @app.route("/")
 def index():
     filename = 'request for startup.csv'
     data = pandas.read_csv(filename, header=0)
-    info = data.values.flatten()
+    info = data.values
     return render_template('index.html', csv_data=info)
 
 
